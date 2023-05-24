49315803716a7dfebccede15315cf2dde77f198a
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-05-24 14:27:07 +0900

    fix layout

--- src/app/photos/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BasicLayout from "@/components/Layouts/BasicLayout";
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <BasicLayout>{children}</BasicLayout>;
-};
-
-export default layout;
